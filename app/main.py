82ebba49f30f454ed0626c855ff27e113c686e98
John Ortiz <jaortiz92@hotmail.com>
2024-10-09 13:40:18 -0500

    Config CORS to use frontend local

--- app/main.py
+++ app/main.py
@@ -16,6 +16,20 @@ app = FastAPI(
     docs_url="/"
 )
 
+# CORS
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Validate Relationships
 try:
