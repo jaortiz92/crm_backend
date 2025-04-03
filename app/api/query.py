a352875502a251ef1b2e32e8e25ac176a1829d88
John Ortiz <jaortiz92@hotmail.com>
2025-04-03 15:53:34 -0500

    Updated query summaries to customers

--- app/api/query.py
+++ app/api/query.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import Session
 from typing import List
 
 # App
-from app.schemas import CustomerSummary, CustomerTripSummary, CollectionSummary
+from app.schemas import CustomerSummary, CustomerTripSummary, CollectionSummary, User
 from app import get_db
 import app.crud as crud
 from app.api.utils import Exceptions
+from app.core.auth import get_current_user
 
 query = APIRouter(
     prefix="/query",
@@ -39,9 +40,15 @@ def get_customer_summary(id_customer_trip: int, db: Session = Depends(get_db)):
 
 
 @query.get("/collection_summary", response_model=List[CollectionSummary])
-def get_collection_summary(db: Session = Depends(get_db)):
+def get_collection_summary(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+    access_type: str = Depends(crud.get_me_access_type)
+):
     """
     Show a summary about collection
     """
-    db_rating = crud.get_collection_summary(db)
+    db_rating = crud.get_collection_summary(
+        db, current_user.id_user, access_type
+    )
     return db_rating
--- app/crud/query.py
+++ app/crud/query.py
@@ -8,6 +8,8 @@ from app.models.query import (
     CustomerTripSummary as CustomerTripSummaryModel,
     CollectionSummary as CollectionSummaryModel
 )
+import app.crud as crud
+from app.crud.utils import Constants
 
 
 def get_customer_summary(db: Session, id_customer: int) -> list[CustomerSummaryModel]:
@@ -24,6 +26,57 @@ def get_customer_trip_summary(db: Session, id_customer_trip: int) -> list[Custom
     return result
 
 
-def get_collection_summary(db: Session) -> list[CollectionSummaryModel]:
-    result = db.query(CollectionSummaryModel).all()
+def get_collection_summary(db: Session,  id_user: int, access_type: str) -> list[CollectionSummaryModel]:
+    auth = Constants.get_auth_to_customers(access_type)
+    result = []
+    if auth == Constants.ALL:
+        query = db.query(
+            CustomerSummaryModel.collection_name,
+            CustomerSummaryModel.short_collection_name,
+            CustomerSummaryModel.year,
+            CustomerSummaryModel.quarter,
+            func.count(CustomerSummaryModel.id_customer
+                       ).label("customers"),
+            func.sum(CustomerSummaryModel.customer_trips
+                     ).label("customer_trips"),
+            func.sum(CustomerSummaryModel.budget
+                     ).label("budget"),
+            func.sum(CustomerSummaryModel.budget_quantities
+                     ).label("budget_quantities"),
+            func.sum(CustomerSummaryModel.orders
+                     ).label("orders"),
+            func.sum(CustomerSummaryModel.order_quantities
+                     ).label("order_quantities"),
+            func.sum(CustomerSummaryModel.order_without_tax
+                     ).label("order_without_tax"),
+            func.sum(CustomerSummaryModel.invoices
+                     ).label("invoices"),
+            func.sum(CustomerSummaryModel.invoice_quantities
+                     ).label("invoice_quantities"),
+            func.sum(CustomerSummaryModel.invoice_without_tax
+                     ).label("invoice_without_tax"),
+            func.sum(CustomerSummaryModel.invoice_discount
+                     ).label("invoice_discount")
+        )
+
+        id_customers = crud.get_id_customers_by_seller(db, id_user)
+        query = query.filter(
+            CustomerSummaryModel.id_customer.in_(id_customers)
+        )
+
+        query = query.group_by(
+            CustomerSummaryModel.collection_name,
+            CustomerSummaryModel.short_collection_name,
+            CustomerSummaryModel.year,
+            CustomerSummaryModel.quarter
+        ).order_by(
+            CustomerSummaryModel.year.desc(),
+            CustomerSummaryModel.quarter.desc(),
+            CustomerSummaryModel.short_collection_name.asc()
+        )
+
+        result = query.all()
+    elif auth == Constants.FILTER:
+        result = db.query(CollectionSummaryModel).all()
+
     return result
