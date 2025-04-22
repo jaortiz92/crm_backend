45bf7c1968d40bce9cbfa3d9a72f0a4f77691784
John Ortiz <jaortiz92@hotmail.com>
2025-04-22 18:50:36 -0500

    Added filed to activityTypes, it can use category by area

--- app/models/activityType.py
+++ app/models/activityType.py
@@ -8,12 +8,14 @@ from sqlalchemy.orm import relationship
 # APP
 from app.db import Base
 
+
 class ActivityType(Base):
     __tablename__ = "activity_types"
 
     id_activity_type = Column(Integer, primary_key=True, index=True)
     activity = Column(String(100), unique=True, index=True, nullable=False)
     mandatory = Column(Boolean, nullable=False)
+    category = Column(String(30), unique=True, index=True, nullable=False)
     activity_order = Column(Integer, nullable=False)
 
     activities = relationship("Activity", back_populates="activity_type")
--- app/schemas/activityType.py
+++ app/schemas/activityType.py
@@ -5,23 +5,35 @@ from typing import Optional, List, Dict
 # Pydantic
 from pydantic import BaseModel, Field
 
+
 class ActivityTypeBase(BaseModel):
-    activity: str = Field(...,
+    activity: str = Field(
+        ...,
         max_length=100,
         description='Activity name (max 100 characters)'
     )
-    mandatory: bool = Field(None,
+    mandatory: bool = Field(
+        None,
         description='Whether the activity is mandatory'
     )
-    activity_order:int = Field(None,
+    category: str = Field(
+        ...,
+        max_length=30,
+        description='Category name (max 30 characters)'
+    )
+    activity_order: int = Field(
+        None,
         description='Order of the activity'
     )
+
 
 class ActivityTypeCreate(ActivityTypeBase):
     pass
 
+
 class ActivityType(ActivityTypeBase):
-    id_activity_type: int = Field(...,
+    id_activity_type: int = Field(
+        ...,
         gt=0
     )
 
